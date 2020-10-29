bcc3155e8aa0920a6bedd2035ce860b9cae51039
Jokerls <1152501640@qq.com>
2020-10-29 11:19:43 +0800

    2020-10-29 添加了 push-test 推送，可发送推送、获取推送消息，待解决：根据推送路径参数跳转具体页面

---
+++ pages/push-test/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<view>
+		<button type="default" @click="pushMsg">createMessage</button>
+		<button type="default" @click="getClientInfo">getClientInfo</button>
+		<button type="default" @click="getAllMessage">getAllMessage</button>
+		<button type="default" @click="clear">clear</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		created() {
+				// 添加监听从系统消息中心点击某条消息启动应用事件
+				plus.push.addEventListener("click", function(msg) {
+					// 分析msg.payload处理业务逻辑
+					console.log(JSON.stringify(msg))
+					console.log("You clicked: " + msg.payload);
+					console.log("You clicked: " + msg.content);
+				}, false);
+		},
+		data() {
+			return {
+				
+			};
+		},
+		
+		methods:{
+			pushMsg() {
+				let str = "这是秋天的第一杯奶茶~"
+				let options = {
+					cover: false,
+					title: "这是标题噢~",
+					subtitle: "这是副标题噢~"
+				}
+			
+				void plus.push.createMessage(str, "LOCALMSG", options)
+			},
+			
+			// 获取客户端推送标识信息
+			getClientInfo() {
+				let info = plus.push.getClientInfo()
+				console.log(info)
+			},
+			
+			// 获取所有推送消息
+			getAllMessage() {
+				let info = plus.push.getAllMessage();
+				console.log(info)
+			},
+			
+			// 清空所有消息
+			clear(){
+			void plus.push.clear();
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
