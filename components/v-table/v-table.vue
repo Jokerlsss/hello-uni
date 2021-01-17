01f79db30f2e93a3f5047f22881c6e8e881ff863
jokerls <1152501640@qq.com>
2021-01-17 11:15:10 +0800

    [新增]wyb-table控件源码；[修改]table-index.vue改架构，表头列名和表格数据一一进行对应

--- components/v-table/v-table.vue
+++ components/v-table/v-table.vue
@@ -2,19 +2,39 @@
 	<view class="container">
 		<!-- 表头 -->
 
-		<view class="slot">
-			<slot></slot>
-		</view>
 		<!-- 行 -->
 		<scroll-view scroll-y="true" style="height:400rpx">
-			<view class="row" :class="((rowIndex%2===1) && isLightRow)?'lightRow':''" v-for="(rowItem,rowIndex) of tableData"
-			 :key="rowIndex">
-				<!-- 列 -->
-				<view class="col" :style="{height:height+'rpx','justify-content':align}" v-for="(colItem,colIndex) of rowItem" :key="colIndex">
-					{{colItem}}
+			<view class="header">
+				<view class="header-item" v-for="(item,index) of headers" :key="item.key">
+					{{item.label}}
+				</view>
+			</view>
+			<view class="content">
+				<view class="row" :class="((rowIndex%2===1) && isLightRow)?'lightRow':''" v-for="(rowItem,rowIndex) of tableData"
+				 :key="contentLineKey(rowItem,rowIndex)">
+					<!-- 列 -->
+					<view class="col" :style="{height:height+'rpx','justify-content':align}" v-for="(header,hIndex) of headers" :key="contentItemKey(header, hIndex)">
+						{{autoContentItem(rowIndex,hIndex)}}
+					</view>
 				</view>
 			</view>
 		</scroll-view>
+
+		<!-- 重构表格架构：可将表头属性和表格内容一一对应 -->
+		<!-- <scroll-view scroll="true">
+			<view class="header">
+				<view class="header-item" v-for="(item,index) of headers" :key="item.key" style="position: sticky;"></view>
+			</view>
+			<view class="content">
+				行
+				<view class="content-line" v-for="(content,cIndex) of contentsSort" :key="contentLineKey(content, cIndex)">
+					列
+					<view class="content-item" v-for="(header,hIndex) of headers"  :key="contentItemKey(header, hIndex)" >
+						{{autoContentItem(cIndex, hIndex)}}
+					</view>
+				</view>
+			</view>
+		</scroll-view> -->
 	</view>
 </template>
 
@@ -23,6 +43,16 @@
 		name: 'v-table',
 		props: {
 			// ------------- 表格主属性 -------------
+			// 表格数据
+			tableData: {
+				type: Array,
+				default: []
+			},
+			// 列头数据
+			headers:{
+				type:Array,
+				default:[]
+			},
 			// 行高
 			height: {
 				type: Number,
@@ -33,11 +63,6 @@
 				type: Number,
 				default: 100
 			},
-			// 表格数据
-			tableData: {
-				type: Array,
-				default: []
-			},
 			// 文本位置
 			align: {
 				type: String,
@@ -47,9 +72,14 @@
 			isLightRow: {
 				type: Boolean,
 				default: true
+			},
+			// 空字符
+			emptyString: {
+				type: String,
+				default: '-'
 			}
 		},
-		
+
 		computed: {
 			textAlign() {
 				switch (this.align) {
@@ -66,7 +96,34 @@
 		},
 		data() {
 			return {
+				chars: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 			};
+		},
+		methods: {
+			// 将表头的属性与表格数据列一一对应
+			autoContentItem(cIndex,hIndex) {
+					let content = this.tableData[cIndex]
+					let header = this.headers[hIndex]
+					let result = ''
+					if (content[header.key] || content[header.key] === 0) {
+						result = content[header.key]
+					} else {
+						result = this.emptyString
+					}
+					return result
+			},
+			// 生成随机key值
+			contentLineKey() {
+					return this.randomString(32, this.chars)
+			},
+			contentItemKey() {
+					return this.randomString(16, this.chars)
+			},
+			randomString(length, chars) {
+				var result = ''
+				for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)]
+				return result
+			}
 		}
 	}
 </script>
--- pages/navigation/index.vue
+++ pages/navigation/index.vue
@@ -41,7 +41,7 @@
 				 :number="itemList.approve.number" :isWarning="itemList.approve.isWarning"></v-main-operate-item>
 
 				<!-- 单据查看 -->
-				<v-main-operate-item :imgSrc="itemList.see.imgSrc" :text="itemList.see.text" @toPage="toPage('see')" :number="itemList.see.number"
+				<v-main-operate-item :imgSrc="itemList.see.imgSrc" :text="itemList.see.text" @toPage="toPage('table')" :number="itemList.see.number"
 				 :isWarning="itemList.see.isWarning"></v-main-operate-item>
 			</view>
 			<view class="operate">
@@ -89,26 +89,28 @@
 						number: '',
 						isWarning: false
 					}
-				}
+				},
+				chars: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 			};
+		},
+		mounted() {
+			this.randomString(16,this.chars)
 		},
 		methods: {
 			// 跳转页面
 			toPage(value) {
-				switch (value) {
-					case 'approve':
-						// 审批
-						break
-					case 'see':
-						// 单据查看
-						break
-					case 'perform':
-						// 公司公告
-						break
-					case 'edit':
-						// 单据编辑
-						break
-				}
+					uni.navigateTo({
+						url: '../'+value+'/index',
+						success: res => {},
+						fail: () => {},
+						complete: () => {}
+					});
+			},
+			randomString(length, chars) {
+			    var result = ''
+			    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)]
+				console.log('result',result);
+			    return result
 			}
 		}
 	}
